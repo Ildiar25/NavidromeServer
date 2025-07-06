9649790a132ebfb1ab62b05ba77e01236cc183dc
Joan <j.pastor1591@gmail.com>
2025-07-06 17:53:23 +0200

    Fix imports according to PEP8

--- addons/music_manager/models/__init__.py
+++ addons/music_manager/models/__init__.py
@@ -1,4 +1,12 @@
+# -*- coding: utf-8 -*-
 from .album import Album
 from .artist import Artist
 from .track import Track
 from .genre import Genre
+
+__all__ = [
+    "Album",
+    "Artist",
+    "Track",
+    "Genre",
+]
