a3c6c29b7ba054e27c1e7c458124444e71b50222
Joan <j.pastor1591@gmail.com>
2025-07-12 20:28:54 +0200

    [REF] services: Refactor metadata service with dataclass TrackMetadata.

--- addons/music_manager/models/track.py
+++ addons/music_manager/models/track.py
@@ -5,17 +5,18 @@ import logging
 
 # noinspection PyPackageRequirements
 import magic
-from pytube.exceptions import RegexMatchError, VideoPrivate, VideoRegionBlocked, VideoUnavailable
 from urllib.parse import urlparse
 
 # noinspection PyProtectedMember
 from odoo import _, api
 from odoo.exceptions import ValidationError
-from odoo.fields import Binary, Boolean, Char, Integer, Many2many, Many2one, Selection
+from odoo.fields import Binary, Boolean, Char, Many2many, Many2one, Selection
 from odoo.models import Model
 
 from ..services.download_service import YTDLPAdapter, YoutubeDownload
 from ..services.metadata_service import MP3File
+from ..utils.exceptions import DownloadServiceError, InvalidMetadataServiceError, MusicManagerError
+
 
 _logger = logging.getLogger(__name__)
 
@@ -28,8 +29,8 @@ class Track(Model):
     # Default fields
     name = Char(string=_("Song title"))
     year = Char(string=_("Year"))
-    track_nu = Integer(string=_("Track no"))
-    disk_nu = Integer(string=_("Disk no"))
+    track_no = Char(string=_("Track no"))
+    disk_no = Char(string=_("Disk no"))
     collection = Boolean(string=_("Part of a collection"))
     duration = Char(string=_("Duration (min)"), readonly=True)
     file = Binary(string=_("File"))
@@ -63,7 +64,10 @@ class Track(Model):
             match track.state:
                 case 'start':
                     track.state = 'uploaded'
-                    # self._update_fields()
+                    if track.url:
+                        self._convert_to_mp3()
+
+                    self._update_fields()
 
                 case 'uploaded':
                     track.state = 'metadata'
@@ -88,21 +92,22 @@ class Track(Model):
                 with open(f"/music/{name}.mp3", "wb") as file_test:
                     file_test.write(picture)
 
-                track.file = False
-
     def save_changes(self) -> None:
         pass
 
     @api.constrains('file', 'url')
     def _check_fields(self) -> None:
         for track in self:
+
             if not track.file and not track.url:
+                _logger.info(f"CONSTRAINT CHECK | file: {bool(track.file)} | url: {bool(track.url)}")
                 raise ValidationError(_("Must add an URL or a file to proceed."))
 
-            # if track.file and track.url:
-            #     raise ValidationError(
-            #         _("Only one file can be added at the same time. Please, delete one of them to continue.")
-            #     )
+            if track.file and track.url:
+                _logger.info(f"CONSTRAINT CHECK | file: {bool(track.file)} | url: {bool(track.url)}")
+                raise ValidationError(
+                    _("Only one file can be added at the same time. Please, delete one of them to continue.")
+                )
 
     @api.constrains('file')
     def _validate_file_type(self) -> None:
@@ -111,8 +116,6 @@ class Track(Model):
 
                 file_data = base64.b64decode(track.file)
                 mime_type = magic.from_buffer(file_data, mime=True)
-
-                _logger.info(f"CONSTRAINT | Bytes length: {len(file_data)} | MIME type: {mime_type}")
 
                 if mime_type not in ["audio/mpeg", "audio/mpg", "audio/x-mpeg"]:
                     raise ValidationError(_("Actually only MP3 files are allowed: %s", mime_type))
@@ -127,30 +130,47 @@ class Track(Model):
                 if not (parsed_url.netloc.endswith('youtube.com') or parsed_url.netloc.endswith('youtu.be')):
                     raise ValidationError(_("The URL must be a valid YouTube URL."))
 
+    def _convert_to_mp3(self) -> None:
+        for track in self:
+            if track.url and isinstance(track.url, str):
                 try:
-
                     buffer = io.BytesIO()
-                    adapter = YTDLPAdapter(track.url)
+                    adapter = YTDLPAdapter(url=track.url)
                     downloader = YoutubeDownload()
 
                     bytes_file = downloader.set_stream_to_buffer(adapter, buffer)
                     mime_type = magic.from_buffer(bytes_file, mime=True)
                     _logger.info(f"Download bytes length: {len(bytes_file)} | MIME type: {mime_type}\n")
 
-                    # track.url = False
-                    track.file = base64.b64encode(bytes_file)
+                    track.write(
+                        {
+                            'url': False,
+                            'file': base64.b64encode(bytes_file)
+                        }
+                    )
 
-                except (RegexMatchError, VideoPrivate, VideoRegionBlocked, VideoUnavailable) as video_error:
-                    _logger.warning(f"Failed to process YouTube URL {track.url}: {video_error}")
+                except DownloadServiceError as video_error:
+                    _logger.error(f"Failed to process YouTube URL {track.url}: {video_error}")
                     raise ValidationError(_("Invalid YouTube URL or video is not accessible."))
 
-                except Exception as unkown_error:
-                    _logger.error(f"Unexpected error while validating URL {track.url}: {unkown_error}")
+                except MusicManagerError as unknown_error:
+                    _logger.error(f"Unexpected error while validating URL {track.url}: {unknown_error}")
                     raise ValidationError(_("Sorry, something went wrong while validating URL."))
 
-    # def _update_fields(self) -> None:
-    #     for track in self:
-    #         track_info = MP3File().get_metadata(io.BytesIO(base64.b64decode(track.file)))
-    #
-    #         track.name = track_info['title']
-    #         track.year = track_info['year']
+    def _update_fields(self) -> None:
+        for track in self:
+            try:
+                metadata = MP3File().get_metadata(io.BytesIO(base64.b64decode(track.file)))
+
+                track.name = metadata.TIT2
+                track.year = metadata.TDRC
+                track.track_no = metadata.TRCK
+                track.disk_no = metadata.TPOS
+
+            except InvalidMetadataServiceError as invalid_metadata:
+                _logger.error(f"Failed to process file metadata: {invalid_metadata}")
+                raise ValidationError(_("Invalid metadata founded on file."))
+
+            except MusicManagerError as unknown_error:
+                _logger.error(f"Unexpected error while processing metadata file: {unknown_error}")
+                raise ValidationError(_("Sorry, something went wrong while loading metadata file."))
--- addons/music_manager/services/metadata_service.py
+++ addons/music_manager/services/metadata_service.py
@@ -7,6 +7,9 @@ import mutagen.id3 as tag_type
 from mutagen.mp3 import MP3
 from mutagen.id3 import ID3
 
+from ..models.metadata import TrackMetadata
+from ..utils.exceptions import InvalidMetadataServiceError, MusicManagerError
+
 
 _logger = logging.getLogger(__name__)
 
@@ -18,7 +21,7 @@ class FileMetadata(ABC):
         ...
 
     @abstractmethod
-    def set_metadata(self, file_path: str | io.BytesIO, new_data: dict) -> None:
+    def set_metadata(self, file_path: str | io.BytesIO, new_data: TrackMetadata) -> None:
         ...
 
     @abstractmethod
@@ -28,84 +31,68 @@ class FileMetadata(ABC):
 
 class MP3File(FileMetadata):
 
-    def get_metadata(self, file_path: str | io.BytesIO) -> dict:
-        file_metadata = {}
-        try:
-            track = MP3(file_path, ID3=ID3)
+    def get_metadata(self, file_path: str | io.BytesIO) -> TrackMetadata:
 
-        except tag_type.ID3NoHeaderError as no_tags:
-            _logger.error(f"No tags founded in this file: {no_tags}")
+        track = self.__load_track(file_path)
 
-        except Exception as unknown:
-            _logger.error(f"Something went wrong while analyzing file metadata: {unknown}")
+        if track.tags:
 
-        else:
-            if track.tags:
-                title = track.tags.get('TIT2')
-                artist = track.tags.get('TPE1')
-                album_artist = track.tags.get('TPE2')
-                album = track.tags.get('TALB')
-                track_info = track.tags.get('TRCK')
-                year = track.tags.get('TDRC')
-                genre = track.tags.get('TCON')
+            metadata = {}
+            metadata_fields = TrackMetadata().__dict__.keys()
 
-                track_data = self._get_track_data(track_info.text[0])
+            for key, value in track.tags.items():
+                if key in metadata_fields and hasattr(value, 'text'):
+                    if key == 'TRCK' and '/' in value.text[0]:
+                        trck, tpos = self.__parse_track_string(value.text[0])
+                        metadata['TRCK'] = trck
+                        metadata['TPOS'] = tpos
+                    else:
+                        metadata[key] = value.text[0]
 
-                file_metadata['title'] = title.text[0] if title else "N/A"
-                file_metadata['artist'] = artist.text[0] if artist else "N/A"
-                file_metadata['album_artist'] = album_artist.text[0] if album_artist else "N/A"
-                file_metadata['album'] = album.text[0] if album else "N/A"
-                file_metadata['track_no'] = track_data[0] if track_data else "1"
-                file_metadata['disk_no'] = track_data[1] if track_data else "1"
-                file_metadata['year'] = year.text[0] if year else "N/A"
-                file_metadata['genre'] = genre.text[0] if genre else "N/A"
+            return TrackMetadata(**metadata)
 
-        return file_metadata
+        return TrackMetadata()
 
-    def set_metadata(self, file_path: str | io.BytesIO, new_data: dict) -> None:
-        try:
-            track = MP3(file_path, ID3=ID3)
-
-        except tag_type.ID3NoHeaderError as no_tags:
-            _logger.error(f"No tags founded in this file: {no_tags}")
-
-        except Exception as unknown:
-            _logger.error(f"Something went wrong while analyzing file metadata: {unknown}")
-
-        else:
-            track.tags.add(tag_type.TIT2(encoding=3, text=[new_data.get('title', "N/A")]))
-            track.tags.add(tag_type.TPE1(encoding=3, text=[new_data.get('artist', "N/A")]))
-            track.tags.add(tag_type.TPE2(encoding=3, text=[new_data.get('album_artist', "N/A")]))
-            track.tags.add(tag_type.TALB(encoding=3, text=[new_data.get('album', "N/A")]))
-            track.tags.add(tag_type.TRCK(encoding=3, text=[
-                self._set_track_data(new_data.get('track_no', ('1', '1')))
-            ]))
-            track.tags.add(tag_type.TDRC(encoding=3, text=[new_data.get('year', "N/A")]))
-            track.tags.add(tag_type.TCON(encoding=3, text=[new_data.get('genre', "N/A")]))
-
-            track.save()
+    def set_metadata(self, file_path: str | io.BytesIO, new_data: TrackMetadata) -> None:
+        pass
 
     def reset_metadata(self, file_path: str | io.BytesIO) -> None:
+
         track = MP3(file_path)
+
         if track.tags:
             track.tags.clear()
         else:
             track.add_tags()
+
         track.save()
 
     @staticmethod
-    def _get_track_data(data: str | None) -> tuple[str, str] | None:
-        if data:
+    def __load_track(file: str | io.BytesIO) -> MP3:
+        try:
+            return MP3(file, ID3=ID3)
+
+        except tag_type.ID3NoHeaderError as no_tags:
+            _logger.error(f"No tags founded in this file: {no_tags}")
+            raise InvalidMetadataServiceError(no_tags)
+
+        except Exception as unknown_error:
+            _logger.error(f"Something went wrong while analyzing file metadata: {unknown_error}")
+            raise MusicManagerError(unknown_error)
+
+    @staticmethod
+    def __parse_track_string(data: str) -> tuple[str, str] | str:
+        if "/" in data:
             track, disk = data.split("/")
             return track, disk
 
         return data
 
     @staticmethod
-    def _set_track_data(track_data: tuple[str, str] | None) -> str | None:
-        if track_data:
-            data = "/".join(track_data)
+    def __format_track_tuple(track_tuple: tuple[str, str] | str) -> str:
+        if isinstance(track_tuple, tuple):
+            data = "/".join(track_tuple)
             return data
 
-        return track_data
+        return track_tuple
 
