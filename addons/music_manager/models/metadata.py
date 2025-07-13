4378672fb59750f91b919e2b050971e0ecbc3f43
Joan <j.pastor1591@gmail.com>
2025-07-13 11:16:21 +0200

    [REF] metadata model: Set disk and track numbers to tuples.

--- addons/music_manager/models/metadata.py
+++ addons/music_manager/models/metadata.py
@@ -3,13 +3,13 @@ from dataclasses import dataclass
 
 @dataclass()
 class TrackMetadata:
-    TIT2: str = "N/A"               # Title
-    TPE1: str = "N/A"               # Track artist
-    TPE2: str = "N/A"               # Album artist
-    TOPE: str = "N/A"               # Original artist
-    TALB: str = "N/A"               # Album
-    TRCK: str = "1"                 # Track no
-    TPOS: str = "1"                 # Disk no
-    TDRC: str = "N/A"               # Year
-    TCON: str = "N/A"               # Genre
-    APIC: bytes | None = None       # Cover
+    TIT2: str = "Unknown"               # Title
+    TPE1: str = "Unknown"               # Track artist
+    TPE2: str = "Unknown"               # Album artist
+    TOPE: str = "Unknown"               # Original artist
+    TALB: str = "Unknown"               # Album
+    TRCK: tuple[str, str] = ("1", "1")  # Track no / Total tracks
+    TPOS: tuple[str, str] = ("1", "1")  # Disk no / Total disks
+    TDRC: str = "Unknown"               # Year
+    TCON: str = "Unknown"               # Genre
+    APIC: bytes | None = None           # Cover
