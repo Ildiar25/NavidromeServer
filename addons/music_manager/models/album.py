6abb704d2e68d6c525580e475a074d583af45abe
Joan <j.pastor1591@gmail.com>
2025-08-12 13:56:34 +0200

    [UPDATE] album model: set computed methods.

--- addons/music_manager/models/album.py
+++ addons/music_manager/models/album.py
@@ -1,22 +1,45 @@
 # -*- coding: utf-8 -*-
+import logging
+
 # noinspection PyProtectedMember
-from odoo import _
+from odoo import _, api
 from odoo.models import Model
-from odoo.fields import Binary, Char, Many2one
+from odoo.fields import Binary, Char, Integer, Many2one, One2many
+
+
+_logger = logging.getLogger(__name__)
 
 
 class Album(Model):
 
     _name = 'music_manager.album'
+    _description = 'album_table'
 
-    # Default fields
+    # Basic fields
     name = Char(string=_("Album title"))
     year = Char(string=_("Year"))
-    total_disks = Char(string=_("Total disks"), readonly=True)
-    cover = Binary(string=_("Cover"), readonly=True)
-    genre = Char(string=_("Genre"), readonly=True)
+    cover = Binary(string=_("Cover"))
 
-    # Relationships
+    # Relational fields
     album_artist_id = Many2one(comodel_name='music_manager.artist', string=_("Album artist"))
     genre_id = Many2one(comodel_name='music_manager.genre', string=_("Genre"))
+    track_ids = One2many(comodel_name='music_manager.track', inverse_name='album_id', string=_("Tracks"))
+
+    # Computed fields
+    disk_amount = Integer(string=_("Disk amount"), compute='_compute_disk_amount', default=0)
+    track_amount = Integer(string=_("Track amount"), compute='_compute_track_amount', default=0)
+
+    # Technical fields
     user_id = Many2one(comodel_name='res.users', string=_("Owner"), default=lambda self: self.env.user)
+
+    @api.depends('track_ids')
+    def _compute_track_amount(self) -> None:
+        for album in self:
+            album.track_amount = len(album.track_ids) if album.track_ids else 0
+
+    @api.depends('track_ids.total_disk')
+    def _compute_disk_amount(self) -> None:
+        for album in self:
+            disk_amount = album.track_ids.mapped('total_disk')
+
+            album.disk_amount = max(disk_amount) if disk_amount else 0
