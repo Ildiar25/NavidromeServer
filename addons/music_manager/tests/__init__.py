8502720028f242ece7de51e4b57a50a070b326f9
Ildiar <j.pastor1591@gmail.com>
2025-11-03 10:54:16 +0100

    [ADDED] tests: Comment init file.

--- addons/music_manager/tests/__init__.py
+++ addons/music_manager/tests/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from . import test_adapter_file_service
-from . import test_service_download_service
+# from . import test_service_download_service
 from . import test_service_file_service
-from . import test_service_image_service
-from . import test_service_metadata_service
+# from . import test_service_image_service
+# from . import test_service_metadata_service
