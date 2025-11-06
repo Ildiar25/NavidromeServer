7c52b97f441adbdc276b84a987fb39cce743bb79
Ildiar <j.pastor1591@gmail.com>
2025-11-06 10:53:20 +0100

    [ADD] utils: Added file utils. Get mime type & raise errors now is available.

---
+++ addons/music_manager/utils/file_utils.py
@@ -0,0 +1,35 @@
+import base64
+
+# noinspection PyPackageRequirements
+import magic
+
+# noinspection PyProtectedMember
+from ..utils.exceptions import InvalidFileFormatError
+
+
+def get_mime_file(file_encoded: bytes) -> str:
+    if not file_encoded:
+        raise InvalidFileFormatError("No file data provided")
+
+    if isinstance(file_encoded, bytes):
+        try:
+            file_encoded = base64.b64decode(file_encoded)
+
+        except Exception:
+            raise InvalidFileFormatError("Invalid base64-encoded file")
+
+    mime_type = magic.from_buffer(file_encoded, mime=True)
+
+    if not mime_type:
+        raise InvalidFileFormatError("Unable to read file type")
+
+    return mime_type
+
+
+def validate_allowed_mimes(file_data: bytes, allowed_mimes: set[str]) -> str:
+    mime_type = get_mime_file(file_data)
+
+    if mime_type not in allowed_mimes:
+        raise InvalidFileFormatError(f"Unsupported file format: '{mime_type}'")
+
+    return mime_type
