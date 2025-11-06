deabb993d08f87257076e4d6e3c31cb1c6e03fad
Ildiar <j.pastor1591@gmail.com>
2025-11-06 10:55:03 +0100

    [REF] image service: Improved service adding generic methods to abstract class & separating save methods depending on image format.

--- addons/music_manager/services/image_service.py
+++ addons/music_manager/services/image_service.py
@@ -2,6 +2,7 @@
 import io
 import logging
 from abc import ABC, abstractmethod
+from typing import Type, TypeVar
 
 from PIL import Image, UnidentifiedImageError
 
@@ -11,28 +12,17 @@ from ..utils.exceptions import ImagePersistenceError, InvalidImageFormatError, M
 _logger = logging.getLogger(__name__)
 
 
+I = TypeVar("I", bound='ImageProcessor')
+
+
 # ---- Image service ---- #
-class ImageFile(ABC):
+class ImageProcessor(ABC):
 
-    def center_image(self) -> 'ImageFile':
-        ...
+    def __init__(self, image: Image.Image) -> None:
+        self._image = image
 
-    def with_size(self, width: int, height: int) -> 'ImageFile':
-        ...
-
-    @abstractmethod
-    def build(self, output_path: str | None = None) -> bytes | None:
-        ...
-
-
-class ImageToPNG(ImageFile):
-
-    def __init__(self, img: io.BytesIO) -> None:
-        self.__img: Image = self.__load_image(img)
-
-    def center_image(self) -> 'ImageFile':
-
-        width, height = self.__img.size
+    def center_image(self) -> I:
+        width, height = self._image.size
         min_dimension = min(width, height)
 
         left = (width - min_dimension) / 2
@@ -40,50 +30,41 @@ class ImageToPNG(ImageFile):
         right = (width + min_dimension) / 2
         bottom = (height + min_dimension) / 2
 
-        self.__img = self.__img.crop((left, top, right, bottom))
-
+        self._image = self._image.crop(box=(left, top, right, bottom))
         return self
 
-    def with_size(self, width: int, height: int) -> 'ImageFile':
-        self.__img = self.__img.resize((width, height))
+    def with_size(self, width: int, height: int) -> I:
+        self._image = self._image.resize(size=(width, height))
         return self
 
-    def build(self, output_path: str | None = None) -> bytes | None:
+    @abstractmethod
+    def to_bytes(self) -> bytes:
+        ...
 
-        if not output_path:
-            buffer = io.BytesIO()
-            self.__img.save(buffer, format='PNG')
-            buffer.seek(0)
-            return buffer.read()
+    @abstractmethod
+    def to_file(self, output_path) -> None:
+        ...
 
+
+class ImageToPNG(ImageProcessor):
+
+    def to_bytes(self) -> bytes:
+        buffer = io.BytesIO()
+        self._image.save(buffer, format='png')
+        buffer.seek(0)
+        return buffer.read()
+
+    def to_file(self, output_path: str) -> None:
         if not output_path.lower().endswith('.png'):
-            raise InvalidImageFormatError("Image must have PNG extension")
+            raise InvalidImageFormatError(f"Image must have 'PNG' extension.")
 
         try:
-            self.__img.save(output_path)
-            return None
+            self._image.save(output_path.lower())
 
         except (PermissionError, FileExistsError) as not_allowed:
-            _logger.error(f"File already exists or no permission to write file: {not_allowed}")
+            _logger.error(f"File already exists or is not allowed to write file: {not_allowed}")
             raise ImagePersistenceError(not_allowed)
 
         except Exception as unknown_error:
             _logger.error(f"Something went wrong while saving image: {unknown_error}")
-            raise MusicManagerError(unknown_error)
-
-    @staticmethod
-    def __load_image(img: io.BytesIO) -> Image:
-        try:
-            return Image.open(img)
-
-        except UnidentifiedImageError as corrupt_file:
-            _logger.error(f"Failed to open image (invalid format or corrupted): {corrupt_file}")
-            raise InvalidImageFormatError(corrupt_file)
-
-        except OSError as system_error:
-            _logger.error(f"An OS error ocurred while reading image: {system_error}")
-            raise MusicManagerError(system_error)
-
-        except Exception as unknown_error:
-            _logger.error(f"Something went wrong while reading image: {unknown_error}")
             raise MusicManagerError(unknown_error)
