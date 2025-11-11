b9bc5157ff95bc6cea4f23fc4ac8c363dc6b28ff
Joan Pastor <147839908+Ildiar25@users.noreply.github.com>
2025-11-11 17:07:37 +0100

    [WIP] Refactor entrypoint.sh to use su for Odoo commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Settings
-DB_NAME="odoo_db"
-DB_USER="odoo_user"
+DB_NAME="${POSTGRES_DB}"
+DB_USER="${POSTGRES_USER}"
 DB_HOST="database"
-DB_PASSWORD="odoo_password"
+DB_PASSWORD="${POSTGRES_PASSWORD}"
 
 echo -e "\n⏳ Waiting for PostgreSQL to start on host '$DB_HOST'..."
 
@@ -22,13 +22,26 @@ done
 echo "✅ PostgreSQL is ready!"
 
 # ⚡ Fix permissions on runtime
-echo -e "\n🔧 Create internal dirs..."
-mkdir -p /var/lib/odoo/.local /home/odoo
+echo -e "\n🔧 Fix permissions on runtime..."
+
+RUNTIME_DIRS=(
+    "/var/lib/odoo"
+    "/mnt/extra-addons"
+    "/home/odoo"
+)
+
+for dir in "${RUNTIME_DIRS[@]}"; do
+    if [ -d "$dir" ]; then
+        echo "   ➡  Setting owner odoo:odoo on $dir"
+        chown -R odoo:odoo "$dir" 2>/dev/null || true
+        chmod -R 755 "$dir" 2>/dev/null || true
+    fi
+done
 
 # Entrypoint with extra commands
 if [ -n "$1" ]; then
     echo -e "\n⚙️  Ejecutando comando personalizado: $*\n"
-    exec odoo -c /etc/odoo/odoo.conf "$@"
+    exec su -s /bin/bash odoo -c /etc/odoo/odoo.conf "$*"
     exit $?
 fi
 
@@ -47,18 +60,18 @@ if [ "$DB_EXIST" = "1" ]; then
 
     if [ "$DB_INIT" = "1" ]; then
         echo -e "✅  Odoo DB already initialized. Starting Odoo...\n"
-        exec odoo -c /etc/odoo/odoo.conf --without-demo=all
+        exec su -s /bin/bash odoo -c /etc/odoo/odoo.conf --without-demo=all
     else
         echo -e "⚠️  DB exists but not initialized. Running -i base...\n"
-        odoo -c /etc/odoo/odoo.conf -d "$DB_NAME" -i base --without-demo=all --stop-after-init
+        su -s /bin/bash odoo -c /etc/odoo/odoo.conf -d "$DB_NAME" -i base --without-demo=all --stop-after-init
         echo -e "\n🚀  Initial DB created. Now starting Odoo normally...\n"
-        exec odoo -c /etc/odoo/odoo.conf --without-demo=all
+        exec su -s /bin/bash odoo -c /etc/odoo/odoo.conf --without-demo=all
     fi
 
 else
     echo -e "🆕  Database not found. Initializing...\n"
-    odoo -c /etc/odoo/odoo.conf -d "$DB_NAME" -i base --without-demo=all --stop-after-init
+    su -s /bin/bash odoo -c /etc/odoo/odoo.conf -d "$DB_NAME" -i base --without-demo=all --stop-after-init
 
     echo "🚀  Initial DB created. Now starting Odoo normally..."
-    exec odoo -c /etc/odoo/odoo.conf --without-demo=all
+    exec su -s /bin/bash odoo -c /etc/odoo/odoo.conf --without-demo=all
 fi
