bdb828bbfad2c872c3b40afca2aeb9d0f9e99a96
Ildiar <j.pastor1591@gmail.com>
2025-11-28 18:28:13 +0100

    [ADD] test: Added download service testing.

--- addons/music_manager/tests/mocks/download_mock.py
+++ addons/music_manager/tests/mocks/download_mock.py
@@ -1,10 +1,12 @@
+import io
 from unittest.mock import MagicMock
 from typing import Optional, Any, TypeVar, Type
 
 from .base_mock_helper import BaseMock
+from ...services.download_service import StreamProtocol
 
 
-E = TypeVar("E", bound=BaseException)
+ExceptionType = TypeVar("ExceptionType", bound=BaseException)
 
 
 class DownloadMock(BaseMock):
@@ -13,39 +15,95 @@ class DownloadMock(BaseMock):
 
     Operations covered:
     -------------------
-
+    - stream_to_buffer
+    - stream_to_file
 
     For each operation, mocks are provided for:
     -------------------------------------------
     - Success case
     """
 
-    # ------------------------- #
-    # PyTube module
-    # ------------------------- #
+    @classmethod
+    def stream_to_buffer_success(cls) -> MagicMock:
+        stream_mock = cls._mock_stream_protocol_helper('stream_to_buffer')
+
+        def write_example_bytes(buffer: io.BytesIO):
+            buffer.write(b"Fake mp3")
+
+        stream_mock.stream_to_buffer.side_effect = write_example_bytes
+        return stream_mock
+
+    @classmethod
+    def stream_to_file_success(cls) -> MagicMock:
+        return cls._mock_stream_protocol_helper('stream_to_file')
+
+    @classmethod
+    def _mock_stream_protocol_helper(
+            cls,
+            method_name: str,
+            return_value: Optional[Any] = None,
+            error_name: Type[ExceptionType] | None = None,
+            message: str | None = None
+    ) -> MagicMock:
+
+        stream_protocol_mock = cls.create_mock(StreamProtocol)
+        method_mock = getattr(stream_protocol_mock, method_name)
+
+        if error_name:
+            method_mock.side_effect = cls.simulate_error(error_name, message)
+
+        else:
+            method_mock.return_value = return_value
+
+        return stream_protocol_mock
 
 
+class PytubeAdapterMock(BaseMock):
+    """
+    Simulates different PyTube behaviours.
 
-    # ------------------------- #
-    # YT-DLP module
-    # ------------------------- #
+    Operations covered:
+    -------------------
+    - stream_to_buffer
+    - stream_to_file
+
+    For each operation, mocks are provided for:
+    -------------------------------------------
+    - Success case
+    -
+    """
 
     @classmethod
     def _mock_pytube_helper(
             cls,
             method_name: str,
             return_value: Optional[Any] = None,
-            error_name: Type[E] | None = None,
+            error_name: Type[ExceptionType] | None = None,
             message: str | None = None
     ) -> MagicMock:
         pass
+
+
+class YTDLPAdapterMock(BaseMock):
+    """
+    Simulates different YTDLP behaviours.
+
+    Operations covered:
+    -------------------
+    - stream_to_buffer
+    - stream_to_file
+
+    For each operation, mocks are provided for:
+    -------------------------------------------
+    - Success case
+    """
 
     @classmethod
     def _mock_ytdpl_helper(
             cls,
             method_name: str,
             return_value: Optional[Any] = None,
-            error_name: Type[E] | None = None,
+            error_name: Type[ExceptionType] | None = None,
             message: str | None = None
     ) -> MagicMock:
         pass
--- addons/music_manager/tests/test_service_download_service.py
+++ addons/music_manager/tests/test_service_download_service.py
@@ -1,11 +1,56 @@
+import io
+from pathlib import Path
 from unittest.mock import patch
 
 from odoo.tests.common import TransactionCase
 
 from .mocks.download_mock import DownloadMock
+from ..services.download_service import YoutubeDownload
 
 
 class TestDownloadService(TransactionCase):
+
+    def setUp(self) -> None:
+        self.buffer = io.BytesIO()
+        self.output_path = Path("/fake/path/dir.mp3")
+        self.downloader = YoutubeDownload()
+
+    def tearDown(self) -> None:
+        pass
+
+    # =========================================================================================
+    # Testing for '__init__'
+    # =========================================================================================
+
+    def test_init_with_downloader(self) -> None:
+        self.assertIsInstance(
+            self.downloader, YoutubeDownload, "Initial downloader must be an 'YoutubeDownload' instance."
+        )
+
+    # =========================================================================================
+    # Testing for 'stream_to_file'
+    # =========================================================================================
+
+    def test_download_to_file_success(self) -> None:
+        stream_mock = DownloadMock.stream_to_file_success()
+
+        self.downloader.set_stream_to_file(stream_mock, self.output_path)
+        stream_mock.stream_to_file.assert_called_once_with(self.output_path)
+
+    # =========================================================================================
+    # Testing for 'stream_to_buffer'
+    # =========================================================================================
+
+    def test_download_to_buffer_success(self) -> None:
+        stream_mock = DownloadMock.stream_to_buffer_success()
+        expected_data = b"Fake mp3"
+
+        result = self.downloader.set_stream_to_buffer(stream_mock, self.buffer)
+        stream_mock.stream_to_buffer.assert_called_once_with(self.buffer)
+        self.assertEqual(result, expected_data)
+
+
+class TestPyTUbeAdapter(TransactionCase):
 
     def setUp(self) -> None:
         pass
@@ -13,5 +58,11 @@ class TestDownloadService(TransactionCase):
     def tearDown(self) -> None:
         pass
 
-    def test_something(self):
-        self.assertEqual(True, True)  # add assertion here
+
+class TestYTDLPAdapter(TransactionCase):
+
+    def setUp(self) -> None:
+        pass
+
+    def tearDown(self) -> None:
+        pass
